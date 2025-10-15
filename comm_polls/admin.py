b0bd309d360c640bb8f679fbab10a753f9d1a024
mikolajed <mikolajedjed@gmail.com>
2025-10-15 22:55:55 +0200

    Register basic model in admin

--- comm_polls/admin.py
+++ comm_polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Poll, Choice, Vote
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_by", "created_at", "start_date", "end_date")
+    inlines = [ChoiceInline]
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "poll", "vote_count")
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("voter", "poll", "choice", "voted_at")
